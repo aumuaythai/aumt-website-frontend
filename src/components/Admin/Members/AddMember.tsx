8c9ef6b4ace7689913f79865a2d61a28e802b260
alkerway <alex@walkeralex.com>
2020-04-20 20:35:03 +1200

    add member component working

--- src/components/Admin/Members/AddMember.tsx
+++ src/components/Admin/Members/AddMember.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
-import { Button, Input, Select, Tooltip } from 'antd'
+import { Button, Input, Select, notification } from 'antd'
+import { AumtMember } from '../../../types'
+import db from '../../../services/db'
 import './AddMember.css'
 
 interface AddMemberProps {}
@@ -16,6 +18,7 @@ interface AddMemberState {
     currentECName: string
     currentECNumber: string
     currentECRelationship: string
+    saving: boolean
 }
 
 export class AddMember extends Component<AddMemberProps, AddMemberState> {
@@ -32,8 +35,17 @@ export class AddMember extends Component<AddMemberProps, AddMemberState> {
             currentIsReturningMember: 'No',
             currentECName: '',
             currentECNumber: '',
-            currentECRelationship: ''
+            currentECRelationship: '',
+            saving: false
         }
+    }
+    generateMemberId = () => {
+        const alphabet = '1234567890qwertyuiopasdfghjklzxcvbnm'
+        let id = ''
+        for (let i = 0; i < 28; i ++) {
+            id += alphabet[Math.floor(Math.random() * alphabet.length)]
+        }
+        return id
     }
     onFirstNameChange = (newName: string) => {
         this.setState({...this.state, currentFirstName: newName})
@@ -72,76 +84,122 @@ export class AddMember extends Component<AddMemberProps, AddMemberState> {
     onECRelationChange = (relation: string) => {
         this.setState({...this.state, currentECRelationship: relation})
     }
+    onSaveClick = () => {
+        const member: AumtMember = {
+            firstName: this.state.currentFirstName,
+            lastName: this.state.currentLastName,
+            preferredName: this.state.currentPreferredName,
+            email: this.state.currentEmail,
+            isUoAStudent: this.state.currentIsUoaStudent,
+            UPI: this.state.currentUpi || '0',
+            membership: this.state.currentMembership,
+            isReturningMember: this.state.currentIsReturningMember,
+            EmergencyContactName: this.state.currentECName,
+            EmergencyContactNumber: this.state.currentECNumber,
+            Relationship: this.state.currentECRelationship,
+            disabled: false,
+            displayName: '',
+            emailVerified: false,
+            password: ''
+        }
+        if (!member.firstName || !member.lastName) {
+            return notification.error({message: 'First and Last Name Required'})
+        } else if (!member.email) {
+            return notification.error({message: 'Email Required'})
+        } else if (!member.isReturningMember) {
+            return notification.error({message: 'You must specify if you are a returning member'})
+        } else if (!member.isUoAStudent) {
+            return notification.error({message: 'You must specify if you are a student'})
+        } else if (!member.EmergencyContactName || !member.EmergencyContactNumber || !member.Relationship) {
+            return notification.error({message: 'All Emergency Contact Details are Required'})
+        }
+        this.setState({
+            ...this.state,
+            saving: true
+        })
+        db.setMember(this.generateMemberId(), member)
+            .then(() => {
+                notification.success({message: 'Saved'})
+            })
+            .catch((err) => {
+                notification.error({message: err.toString()})
+            })
+            .finally(() => {
+                this.setState({
+                    ...this.state,
+                    saving: false
+                })
+            })
+    }
     render() {
         return (
-            <div>
-                <div className="memberDescriptionSection">
-                    <h4>Name</h4>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>First: </span>
-                        <Input className='memberEditInput' value={this.state.currentFirstName} onChange={e => this.onFirstNameChange(e.target.value)}/>
+            <div className='addMemberFormContainer'>
+                <div className="addMemberSection">
+                    <h3 className='addMemberSectionHeader'>Your Details</h3>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>First Name<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentFirstName} onChange={e => this.onFirstNameChange(e.target.value)}/>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Last: </span>
-                        <Input className='memberEditInput' value={this.state.currentLastName} onChange={e => this.onLastNameChange(e.target.value)}/>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>Last Name<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentLastName} onChange={e => this.onLastNameChange(e.target.value)}/>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Preferred: </span>
-                        <Input className='memberEditInput' value={this.state.currentPreferredName} onChange={e => this.onPreferredNameChange(e.target.value)}/>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>Preferred Name (if different from First Name): </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentPreferredName} onChange={e => this.onPreferredNameChange(e.target.value)}/>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Email: </span>
-                        <Input className='memberEditInput longMemberEditInput' value={this.state.currentEmail} onChange={e => this.onEmailChange(e.target.value)}/>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>Email<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput longAddMemberFieldInput' value={this.state.currentEmail} onChange={e => this.onEmailChange(e.target.value)}/>
                     </div>
                 </div>
-                <div className="memberDescriptionSection">
-                    <h4>Details</h4>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>UoA Student: </span>
+                <div className="addMemberSection">
+                    <div className='addMemberLine'>
+                        <span className='addMemberFieldTitle'>Are you a Student at the University of Auckland?<span className='requiredDot'>*</span> </span>
                         <Select value={this.state.currentIsUoaStudent} onChange={this.onIsUoaChange} style={{ width: 120 }}>
                             <Select.Option value="Yes">Yes</Select.Option>
                             <Select.Option value="No">No</Select.Option>
                         </Select>
                     </div>
                     <div className={`memberDescriptionLine ${this.state.currentIsUoaStudent === 'Yes' ? '' : 'noHeight'}`}>
-                        <span className='memberDescriptionTitle'>UPI: </span>
-                        <Input className='memberEditInput' value={this.state.currentUpi} onChange={e => this.onUpiChange(e.target.value)}/>
+                        <span className='addMemberFieldTitle'>UPI<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentUpi} onChange={e => this.onUpiChange(e.target.value)}/>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Membership: </span>
-                        <Select value={this.state.currentMembership || 'None'} onChange={this.onMembershipChange} style={{ width: 120 }}>
-                            <Select.Option value="S1">S1</Select.Option>
-                            <Select.Option value="S2">S2</Select.Option>
-                            <Select.Option value="FY">FY</Select.Option>
-                            <Select.Option value='None'>None</Select.Option>
+                    <div className='addMemberLine'>
+                        <span className='addMemberFieldTitle'>Select Membership Duration<span className='requiredDot'>*</span>: </span>
+                        <Select value={this.state.currentMembership || 'None'} onChange={this.onMembershipChange} style={{ width: 220 }}>
+                            <Select.Option value="S1">Semester 1</Select.Option>
+                            <Select.Option value="S2">Semester 2</Select.Option>
+                            <Select.Option value="FY">Full Year</Select.Option>
                         </Select>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Returning Member: </span>
+                    <div className='addMemberLine'>
+                        <span className='addMemberFieldTitle'>Are you a Returning Member?<span className='requiredDot'>*</span></span>
                         <Select value={this.state.currentIsReturningMember} onChange={this.onIsReturningChange} style={{ width: 120 }}>
                             <Select.Option value="Yes">Yes</Select.Option>
                             <Select.Option value="No">No</Select.Option>
                         </Select>
                     </div>
                 </div>
-                <div className="memberDescriptionSection">
-                    <h4>Emergency Contact</h4>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Name: </span>
-                        <Input className='memberEditInput' value={this.state.currentECName} onChange={e => this.onECNameChange(e.target.value)}/>
+                <div className="addMemberSection">
+                    <h3 className='addMemberSectionHeader'>Emergency Contact Details</h3>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>Name<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentECName} onChange={e => this.onECNameChange(e.target.value)}/>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Number: </span>
-                        <Input className='memberEditInput' value={this.state.currentECNumber} onChange={e => this.onECNumberChange(e.target.value)}/>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>Number<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentECNumber} onChange={e => this.onECNumberChange(e.target.value)}/>
                     </div>
-                    <div className='memberDescriptionLine'>
-                        <span className='memberDescriptionTitle'>Relation: </span>
-                        <Input className='memberEditInput' value={this.state.currentECRelationship} onChange={e => this.onECRelationChange(e.target.value)}/>
+                    <div className='addMemberLine shortMemberLine'>
+                        <span className='addMemberFieldTitle'>Relation<span className='requiredDot'>*</span>: </span>
+                        <Input className='addMemberFieldInput' value={this.state.currentECRelationship} onChange={e => this.onECRelationChange(e.target.value)}/>
                     </div>
+                    <div className="clearBoth"></div>
                 </div>
-                {/* <div className="memberDescriptionSection">
+                <div className="addMemberSection">
                     <Button type='primary' loading={this.state.saving} onClick={this.onSaveClick}>Save</Button>
-                </div> */}
+                </div>
             </div>
         )
     }
