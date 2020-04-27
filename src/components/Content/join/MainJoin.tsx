75f349573a381ced55963ed1f5cd2466cf9d2c44
alkerway <alex@walkeralex.com>
2020-04-27 17:32:28 +1200

    remove sign out to join button on join tab if form is closed

--- src/components/Content/join/MainJoin.tsx
+++ src/components/Content/join/MainJoin.tsx
@@ -24,6 +24,22 @@ export class MainJoin extends Component<MainJoinProps, MainJoinState> {
     render() {
         if (this.props.clubSignupStatus === 'loading' || this.props.loadingAuthedUser) {
             return <Spin/>
+        } else if (this.props.authedUser) {
+            return (
+                <Result
+                    status='success'
+                    title='You are a member of AUMT!'
+                    subTitle={this.props.authedUser.paid === 'Yes' ? 'Our records show you have paid, you are all set to sign up for trainings'
+                    : 'Please pay the membership fee to finalise your membership.'}
+                    extra={
+                        this.props.clubSignupStatus === 'open' ?
+                        [
+                        <p key='1'><Button type='link' className='joinResultSignOut' onClick={this.onSignOutClick}>
+                            Sign Out</Button> to go back to the Join form
+                            </p>
+                        ] : []}
+                ></Result>
+            )
         } else if (this.props.clubSignupStatus === 'closed') {
             return (
                 <div>
@@ -33,17 +49,7 @@ export class MainJoin extends Component<MainJoinProps, MainJoinState> {
         }
         return (
             <div>
-                {this.props.authedUser ?
-                <Result
-                    status='success'
-                    title='You are a member of AUMT!'
-                    subTitle={this.props.authedUser.paid === 'Yes' ? 'Our records show you have paid, you are all set to sign up for trainings'
-                    : 'Please pay the membership fee to finalise your membership.'}
-                    extra={[
-                        <p key='1'><Button type='link' className='joinResultSignOut' onClick={this.onSignOutClick}>Sign Out</Button> to go back to the Join form</p>
-                    ]}
-                ></Result>
-                 : <JoinForm isAdmin={false}></JoinForm>}
+                <JoinForm isAdmin={false}></JoinForm>
             </div>
         )
     }
