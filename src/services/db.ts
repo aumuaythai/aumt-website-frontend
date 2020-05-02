de31f5eef9384fda05f2789ddace09e777039461
alkerway <alex@walkeralex.com>
2020-05-03 11:25:15 +1200

    warning for dev firebase sdk in console gone

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
 import { AumtMember, AumtWeeklyTraining, AumtTrainingSession, AumtEvent, AumtMembersObj, ClubConfig } from '../types';
 
 type MockMember = {
