00aee97b853e9ad49ed3727e5c812b28885c0aec
alkerway <alex@walkeralex.com>
2020-08-16 21:54:30 +1200

    addd text for non members

--- src/components/Content/events/Event.tsx
+++ src/components/Content/events/Event.tsx
@@ -212,7 +212,7 @@ export class Event extends Component<EventProps, EventState> {
                                     this.props.event.signups.needAdminConfirm ? 'Our records show you have paid, your spot is confirmed' : ''}
                                     extra={this.state.confirmedSignUp ? [] : 
                                         [
-                                            <p key='xtra' className='joinAccountLine'>The fee is $180 and should be paid with your full name as the reference to: 06-0158-0932609-00
+                                            <p key='xtra' className='joinAccountLine'>The fee is $180 for members and $220 for nonmembers and should be paid with your full name as the reference to: 06-0158-0932609-00
                                             <Button type='link' onClick={e => this.copyText('06-0158-0932609-00')}>Copy Account Number</Button></p>
                                         ]
                                     }
