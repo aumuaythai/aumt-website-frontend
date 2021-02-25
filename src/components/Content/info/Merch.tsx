4272427fe645af94273d81c78d43a66c709f2d1c
4hgann <henry.gann@gmail.com>
2021-02-25 21:13:46 +1300

    Used class names instead of general tags in CSS

--- src/components/Content/info/Merch.tsx
+++ src/components/Content/info/Merch.tsx
@@ -17,29 +17,29 @@ export class Merch extends Component {
 
                 <div className = "contentContainer">
                     <div className = "tshirtContainer">
-                        <h2><span className = "tshirtHeading">T-Shirt</span></h2>
-                        <p>$20</p>
-                        <p>This shirt has the AUMT logo on the front and a custom design on the back depicting a dragon</p>
-                        <p>The shirt is made of polyester which is highly breathable and ideal for training</p>
-                        <p>Available in sizes S-XXXL</p>
+                        <h2><span className = "itemHeading">T-Shirt</span></h2>
+                        <p className = "plainText">$20</p>
+                        <p className = "plainText">This shirt has the AUMT logo on the front and a custom design on the back depicting a dragon</p>
+                        <p className = "plainText">The shirt is made of polyester which is highly breathable and ideal for training</p>
+                        <p className = "plainText">Available in sizes S-XXXL</p>
                     </div>
 
                     <div className = "wrapsContainer">
-                        <h2><span className = "wrapsHeading">Wrist Wraps</span></h2>
-                        <p>$10-25</p>
-                        <p>Wrist wraps are worn in order to protect and support the wrist during punches</p>
-                        <p>Wraps come in many different colours and patterns, the length may also vary</p>
-                        <p>Usually made using a blend of elastic gauze and cotton</p>
+                        <h2><span className = "itemHeading">Wrist Wraps</span></h2>
+                        <p className = "plainText">$10-25</p>
+                        <p className = "plainText">Wrist wraps are worn in order to protect and support the wrist during punches</p>
+                        <p className = "plainText">Wraps come in many different colours and patterns, the length may also vary</p>
+                        <p className = "plainText">Usually made using a blend of elastic gauze and cotton</p>
                         
 
                     </div>
 
                     <div className = "shortsContainer">
-                        <h2><span className = "shortsHeading">Shorts</span></h2>
-                        <p>$45</p>
-                        <p>This style of shorts are traditionally worn by Muay Thai fighters during training and fighting</p>
-                        <p>These shorts allow for good range of motion and are made of a nylon and polyester blend which is ideal for training </p>
-                        <p>Available in sizes S-XXXXL</p>
+                        <h2><span className = "itemHeading">Shorts</span></h2>
+                        <p className = "plainText">$45</p>
+                        <p className = "plainText">This style of shorts are traditionally worn by Muay Thai fighters during training and fighting</p>
+                        <p className = "plainText">These shorts allow for good range of motion and are made of a nylon and polyester blend which is ideal for training </p>
+                        <p className = "plainText">Available in sizes S-XXXXL</p>
                     </div>
                 </div>
             </div>
