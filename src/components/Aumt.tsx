7af1432b73693540ad6f5f50ccf59411379c0739
Grant Liu <100253339+grantliunz@users.noreply.github.com>
2024-02-27 22:14:47 +1300

    update faq to use club config

--- src/components/Aumt.tsx
+++ src/components/Aumt.tsx
@@ -1,13 +1,13 @@
 import React, { lazy, Component, Suspense } from 'react'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import {notification, Spin} from 'antd'
+import { notification, Spin } from 'antd'
 import firebase from 'firebase/app'
 import Header from './Header/Header'
-import {LoginForm} from './Header/LoginForm'
-import {About} from './Content/info/About'
+import { LoginForm } from './Header/LoginForm'
+import { About } from './Content/info/About'
 import EventsWrapper from './Content/events/EventsWrapper'
-import {Faq} from './Content/info/Faq'
-import {Merch} from './Content/info/Merch'
+import { Faq } from './Content/info/Faq'
+import { Merch } from './Content/info/Merch'
 import { MainJoin } from './Content/join/MainJoin'
 import { Account } from './Content/account/Account'
 import { ErrorBoundary } from './Content/error/ErrorBoundary'
@@ -26,7 +26,7 @@ const MainAdminLazyWrapper = (
 
 const TeamLazyWrapper = (
   lazy(() => (
-    import ('./Content/info/Team' /* webpackChunkName: "team" */)
+    import('./Content/info/Team' /* webpackChunkName: "team" */)
   ))
 )
 
@@ -41,60 +41,60 @@ export interface AumtProps {
 }
 
 export interface AumtState {
-    loadingAuthedUser: boolean
-    authedUser: AumtMember | null
-    userIsAdmin: boolean
-    authedUserId: string
-    clubSignupStatus: 'open' | 'closed' | 'loading'
-    clubSignupSem: 'S1' | 'S2' | 'loading' | 'SS'
-    clubConfig: ClubConfig | null
+  loadingAuthedUser: boolean
+  authedUser: AumtMember | null
+  userIsAdmin: boolean
+  authedUserId: string
+  clubSignupStatus: 'open' | 'closed' | 'loading'
+  clubSignupSem: 'S1' | 'S2' | 'loading' | 'SS'
+  clubConfig: ClubConfig | null
 }
 
 export class Aumt extends Component<AumtProps, AumtState> {
-    constructor(props: AumtProps) {
-        super(props)
-        const authedUser = null
-        this.state = {
-          authedUser,
-          loadingAuthedUser: true,
-          userIsAdmin: false,
-          authedUserId: '',
-          clubSignupStatus: 'loading',
-          clubSignupSem: 'loading',
-          clubConfig: null
-        }
-        
-        // Initilize firebase products.
-        FirebaseUtil.initialize(this.authStateChange)
-        Analytics.initialize();
-        Functions.initialize();
-        DB.initialize()
-
-        DB.getClubConfig()
-          .then((config) => {
-            this.setState({
-              ...this.state,
-              clubSignupStatus: config.clubSignupStatus,
-              clubSignupSem: config.clubSignupSem,
-              clubConfig: {...config}
-            })
-          })
-          .catch((err) => {
-            notification.error({message: 'Failed to get website config: ' + err.toString()})
-          })
+  constructor(props: AumtProps) {
+    super(props)
+    const authedUser = null
+    this.state = {
+      authedUser,
+      loadingAuthedUser: true,
+      userIsAdmin: false,
+      authedUserId: '',
+      clubSignupStatus: 'loading',
+      clubSignupSem: 'loading',
+      clubConfig: null
     }
 
-    private authStateChange = (fbUser: firebase.User | null) => {
-      if (fbUser) {
-        DB.getUserInfo(fbUser)
-          .then((userInfo: AumtMember) => {
-            this.setState({
-              ...this.state,
-              authedUser: userInfo,
-              loadingAuthedUser: false,
-              authedUserId: fbUser.uid
-            })
-            return DB.getIsAdmin(fbUser.uid)
+    // Initilize firebase products.
+    FirebaseUtil.initialize(this.authStateChange)
+    Analytics.initialize();
+    Functions.initialize();
+    DB.initialize()
+
+    DB.getClubConfig()
+      .then((config) => {
+        this.setState({
+          ...this.state,
+          clubSignupStatus: config.clubSignupStatus,
+          clubSignupSem: config.clubSignupSem,
+          clubConfig: { ...config }
+        })
+      })
+      .catch((err) => {
+        notification.error({ message: 'Failed to get website config: ' + err.toString() })
+      })
+  }
+
+  private authStateChange = (fbUser: firebase.User | null) => {
+    if (fbUser) {
+      DB.getUserInfo(fbUser)
+        .then((userInfo: AumtMember) => {
+          this.setState({
+            ...this.state,
+            authedUser: userInfo,
+            loadingAuthedUser: false,
+            authedUserId: fbUser.uid
+          })
+          return DB.getIsAdmin(fbUser.uid)
         })
         .then((isAdmin: boolean) => {
           this.setState({
@@ -123,108 +123,108 @@ export class Aumt extends Component<AumtProps, AumtState> {
           return FirebaseUtil.signOut()
         })
         .catch((err) => {
-          notification.error({message: `error with email verified ${err}`})
+          notification.error({ message: `error with email verified ${err}` })
         })
-      } else {
-        this.setState({
-          ...this.state,
-          authedUser: null,
-          authedUserId: '',
-          loadingAuthedUser: false,
-          userIsAdmin: false
-        })
-      }
+    } else {
+      this.setState({
+        ...this.state,
+        authedUser: null,
+        authedUserId: '',
+        loadingAuthedUser: false,
+        userIsAdmin: false
+      })
     }
+  }
 
-    render() {
-        return (
-            <div className="App">
-              <BrowserRouter>
-                <Switch>
-                  <Route path="/login">
-                    <LoginForm></LoginForm>
-                  </Route>
-                  <Route path="/*">
-                    <Header authedUser={this.state.authedUser} isAdmin={this.state.userIsAdmin}></Header>
-                    <ErrorBoundary>
-                      <Suspense fallback={<div><Spin/></div>}>
-                        <Switch>
-                          <Route path="/about">
-                            <Redirect to='/'/>
-                          </Route>
-                          <Route path="/team">
-                            <TeamLazyWrapper></TeamLazyWrapper>
-                          </Route>
-                          <Route path="/faq">
-                            <Faq></Faq>
-                          </Route>
-                          <Route path = "/merch">
-                            <Merch></Merch>
-                          </Route>
-                          <Route path="/signup">
-                            <Redirect to="/signups"/>
-                          </Route>
-                          <Route path="/signups">
-                            <SignupsLazyWrapper
-                              paid={this.state.authedUser?.paid === 'Yes'}
-                              authedUserId={this.state.authedUserId}
-                              clubSignupSem={this.state.clubSignupSem}
-                              authedUser={this.state.authedUser}
-                              clubConfig={this.state.clubConfig}></SignupsLazyWrapper>
-                          </Route>
-                          <Route path="/events">
-                            {/* {this.state.authedUser ?  */}
-                            <EventsWrapper authedUser={this.state.authedUser}></EventsWrapper>
-                            {/* : 
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login">
+              <LoginForm></LoginForm>
+            </Route>
+            <Route path="/*">
+              <Header authedUser={this.state.authedUser} isAdmin={this.state.userIsAdmin}></Header>
+              <ErrorBoundary>
+                <Suspense fallback={<div><Spin /></div>}>
+                  <Switch>
+                    <Route path="/about">
+                      <Redirect to='/' />
+                    </Route>
+                    <Route path="/team">
+                      <TeamLazyWrapper></TeamLazyWrapper>
+                    </Route>
+                    <Route path="/faq">
+                      <Faq ></Faq>
+                    </Route>
+                    <Route path="/merch">
+                      <Merch></Merch>
+                    </Route>
+                    <Route path="/signup">
+                      <Redirect to="/signups" />
+                    </Route>
+                    <Route path="/signups">
+                      <SignupsLazyWrapper
+                        paid={this.state.authedUser?.paid === 'Yes'}
+                        authedUserId={this.state.authedUserId}
+                        clubSignupSem={this.state.clubSignupSem}
+                        authedUser={this.state.authedUser}
+                        clubConfig={this.state.clubConfig}></SignupsLazyWrapper>
+                    </Route>
+                    <Route path="/events">
+                      {/* {this.state.authedUser ?  */}
+                      <EventsWrapper authedUser={this.state.authedUser}></EventsWrapper>
+                      {/* : 
                             <div>
                               You must <Link to='/login?from=/events'> log in </Link> to view events.
                             </div>
                             } */}
-                          </Route>
-                          <Route path="/join">
-                            <MainJoin
-                              loadingAuthedUser={this.state.loadingAuthedUser}
-                              authedUser={this.state.authedUser}
-                              authedUserId={this.state.authedUserId}
-                              clubConfig={this.state.clubConfig}></MainJoin>
-                          </Route>
-                          <Route path="/admin">
-                            {
-                              this.state.userIsAdmin ?
-                                <MainAdminLazyWrapper></MainAdminLazyWrapper> :
-                                <div>
-                                  You are not authorised to access this page.
-                                </div>
-                            }
-                          </Route>
-                          <Route path="/account">
-                            {
-                              this.state.authedUser ?
-                                <Account
-                                 clubSignupSem={this.state.clubSignupSem}
-                              loadingAuthedUser={this.state.loadingAuthedUser}
-                              clubSignupStatus={this.state.clubSignupStatus}
-                              authedUser={this.state.authedUser}
-                              authedUserId={this.state.authedUserId}
-                              clubConfig={this.state.clubConfig}></Account> :
-                                <div>
-                                  You do not have an account yet. Please join.
-                                </div>
-                            }
-                          </Route>
-                          <Route path="/penis">
-                            <img className='headshotheadshot' src="./photos/tom.jpg" alt="Tom Haliday"/>
-                          </Route>
-                          <Route path="/">
-                            <About></About>
-                          </Route>
-                        </Switch>
-                      </Suspense>
-                    </ErrorBoundary>
-                  </Route>
-                </Switch>
-              </BrowserRouter>
-            </div>
-          );
-    }
+                    </Route>
+                    <Route path="/join">
+                      <MainJoin
+                        loadingAuthedUser={this.state.loadingAuthedUser}
+                        authedUser={this.state.authedUser}
+                        authedUserId={this.state.authedUserId}
+                        clubConfig={this.state.clubConfig}></MainJoin>
+                    </Route>
+                    <Route path="/admin">
+                      {
+                        this.state.userIsAdmin ?
+                          <MainAdminLazyWrapper></MainAdminLazyWrapper> :
+                          <div>
+                            You are not authorised to access this page.
+                          </div>
+                      }
+                    </Route>
+                    <Route path="/account">
+                      {
+                        this.state.authedUser ?
+                          <Account
+                            clubSignupSem={this.state.clubSignupSem}
+                            loadingAuthedUser={this.state.loadingAuthedUser}
+                            clubSignupStatus={this.state.clubSignupStatus}
+                            authedUser={this.state.authedUser}
+                            authedUserId={this.state.authedUserId}
+                            clubConfig={this.state.clubConfig}></Account> :
+                          <div>
+                            You do not have an account yet. Please join.
+                          </div>
+                      }
+                    </Route>
+                    <Route path="/penis">
+                      <img className='headshotheadshot' src="./photos/tom.jpg" alt="Tom Haliday" />
+                    </Route>
+                    <Route path="/">
+                      <About></About>
+                    </Route>
+                  </Switch>
+                </Suspense>
+              </ErrorBoundary>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
--- src/components/Content/info/Faq.tsx
+++ src/components/Content/info/Faq.tsx
@@ -1,79 +1,103 @@
-import React, {Component} from 'react'
-import { Link } from 'react-router-dom'
-import {Divider} from 'antd'
-import { Links } from '../../../services/links'
-import './Faq.css'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Divider } from 'antd';
+import { Links } from '../../../services/links';
+import './Faq.css';
+import db from '../../../services/db';
+import { ClubConfig } from '../../../types/ClubConfig';
 
-export class Faq extends Component {
-    fbClick = () => {
-        Links.openAumtFb()
+interface FaqState {
+    clubConfig: ClubConfig | null;
+}
+
+export class Faq extends Component<{}, FaqState> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            clubConfig: null
+        };
     }
-    igClick = () => {
-        Links.openAumtInsta()
+
+    componentDidMount(): void {
+        db.getClubConfig().then((clubConfig: ClubConfig) => {
+            this.setState({
+                clubConfig: clubConfig
+            });
+        }).catch((error: any) => {
+            console.error('Error fetching club configuration:', error);
+        });
     }
-    mapClick = () => {
-        Links.openAumtAddress()
-    }
+
+    fbClick = (): void => {
+        Links.openAumtFb();
+    };
+
+    igClick = (): void => {
+        Links.openAumtInsta();
+    };
+
+    mapClick = (): void => {
+        Links.openAumtAddress();
+    };
+
     render() {
         return (
             <div className='faqContainer'>
-                <div className="questionContainer">
+                <div className="questionContainer" key="trainings">
                     <p className="question">Q: When and where are trainings?</p>
                     <p className="answer">
-                    A: Training times:
-                    <ul>
-                        <li>Beginner: 4:30pm Wednesday, 4:30pm Thursday, 7:30pm Friday</li>
-                        <li>Intermediate: 4:30pm Tuesday, 6:30pm Friday</li>
-                        <li>Advanced: 5:30pm Thursday</li>
-                    </ul>
+                        A: Training times:
+                        <ul>
+                            <li>Beginner: 4:30pm Wednesday, 4:30pm Thursday, 7:30pm Friday</li>
+                            <li>Intermediate: 4:30pm Tuesday, 6:30pm Friday</li>
+                            <li>Advanced: 5:30pm Thursday</li>
+                        </ul>
                     </p>
                     <p>
                         Trainings are located a The Hawks Nest Gym,&nbsp;<span className='socialLink' onClick={this.mapClick}> 492 Queen Street</span>
 
                     </p>
                     <iframe title="Training Location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d633.4536252469256!2d174.76206034919215!3d-36.856698459636775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47bb9877fca5%3A0x70e64b98b3aeed38!2sThe%20Hawks&#39;%20Nest%20Gym!5e0!3m2!1sen!2snz!4v1644916967606!5m2!1sen!2snz" loading="lazy" />
-                    <Divider/>
+                    <Divider />
                 </div>
-                <div className="questionContainer">
-                    <p className="question">
-                        Q: How much is a membership?
-                    </p>
+                <div className="questionContainer" key="membership">
+                    <p className="question">Q: How much is a membership?</p>
                     <p className="answer">
-                        A: Memberships are $50 for one semester or $90 for the full year
+                        A: Memberships are ${this.state.clubConfig?.semesterOneFee} for one semester or ${this.state.clubConfig?.fullYearFee} for the full year
                     </p>
-                    <Divider/>
+                    <Divider />
                 </div>
-                <div className="questionContainer">
-                    <p className="question">
-                        Q: How do I join?
+                <div className="questionContainer" key="bankAcc">
+                    <p className="question">Q: What is the AUMT bank account?</p>
+                    <p className="answer">
+                        A: {this.state.clubConfig?.bankAccountNumber}
                     </p>
+                    <Divider />
+                </div>
+                <div className="questionContainer" key="join">
+                    <p className="question">Q: How do I join?</p>
                     <p className="answer">
                         A: Club signups are only open at the beginning of each semester. When they open, you can join at <Link to='/join'>aumt.co.nz/join</Link>.
                     </p>
-                    <Divider/>
+                    <Divider />
                 </div>
-                <div className="questionContainer">
-                    <p className="question">
-                        Q: What do I need to bring?
-                    </p>
+                <div className="questionContainer" key="bring">
+                    <p className="question">Q: What do I need to bring?</p>
                     <p className="answer">
                         A: Just a comfortable shirt and shorts. All gloves and pads are provided by the club. Having handwraps is highly recommended, as well as a mouth guard for advanced members. These can be purchased from the Hawk’s Nest Gym. You’re welcome to bring your own gear if you prefer.
                     </p>
-                    <Divider/>
+                    <Divider />
                 </div>
-                <div className="questionContainer">
-                    <p className="question">
-                        Q: I want to represent the club. Where can I get some sweet sweet merch?
-                    </p>
+                <div className="questionContainer" key="merchandise">
+                    <p className="question">Q: I want to represent the club. Where can I get some sweet sweet merch?</p>
                     <p className="answer">
-                        A: Talk to your nearest committee member! We have AUMT muay thai shorts and a club shirt as well. 
+                        A: Talk to your nearest committee member! We have AUMT muay thai shorts and a club shirt as well.
                     </p>
-                    <Divider/>
+                    <Divider />
                 </div>
-                <div className="questionContainer">
-                    <p className="question">
-                        Q: Who do I contact if I have additional questions?
-                    </p>
+                <div className="questionContainer" key="contact">
+                    <p className="question">Q: Who do I contact if I have additional questions?</p>
                     <p className="answer">
                         A: The committee members will be more than happy to answer if you approach them at trainings or events or email them at
                         <a href='mailto:uoamuaythai@gmail.com'> uoamuaythai@gmail.com, </a>
@@ -83,9 +107,9 @@ export class Faq extends Component {
                         <span className='socialLink' onClick={this.igClick}>Instagram</span>
                         &nbsp;page
                     </p>
-                    <Divider/>
+                    <Divider />
                 </div>
             </div>
-        )
+        );
     }
 }
