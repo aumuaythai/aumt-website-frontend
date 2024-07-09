cb8cd43d85041a31a5f73a5e4df7459b9197964c
Grant Liu <100253339+grantliunz@users.noreply.github.com>
2024-07-09 13:49:15 +1200

    about page

--- src/components/Content/info/About.tsx
+++ src/components/Content/info/About.tsx
@@ -9,19 +9,100 @@ export class About extends Component {
         return (
             <div className='aboutContainer'>
                 <h1 className="introTitle">
-                    Welcome to <br/>Auckland University Muay Thai
+                    WELCOME TO <br/>AUCKLAND UNIVERSITY MUAY THAI
                 </h1>
-                <div className="groupHeaderContainer">
-                    <img src="./photos/content/group2.jpg" alt="group" className='absoluteResponsiveImg'/>
-                </div>
+                <img src="./photos/content/Landing.jpg" alt="group" className="landingImg"/>
                 <div className="introContainer">
-                    <h3 className='aboutSectionHead'>AUMT aims to introduce the UoA community to the art of Muay Thai.</h3>
-                    <p className="introStr">
-                     Run by a passionate group of Muay Thai enthusiasts, we organise sessions where members can learn more about the sport and pick up some self-defence skills in a safe, supportive environment.
+                    <h3 className='aboutSectionHead'>AUMT INTODUCES STUDENTS TO THE ART OF MUAY THAI</h3>
+                    <p>
+                      Run by a passionate group of Muay Thai Enthusiasts, we run weekly trainings and social events. <br/>
+                      we aim to teach martial arts in a friendly and low-pressure environment.<br/>
+                      All experience levels are welcome!
                     </p>
                 </div>
-                <Divider className='aboutDivider'/>
-                <div className="aboutDescriptionRow">
+                <div className="btnContainer">
+                  <button className="boldBtn teal"><a href='/signups'>TRAININGS</a>  </button>
+                  <button className="boldBtn teal"><a href = '/events'>EVENTS</a> </button>
+                </div>
+                <div className="introContainer">
+                    <h3 className='aboutSectionHead'>WHAT IS MUAY THAI?</h3>
+                    <p>
+                    Known as the Art of Eight Limbs, this martial art discipline from Thailand is characterized by combat <br/> utilising the fists, elbows, knees and shins. It is a highly effective martial art and great for self-defence.
+
+                    </p>
+                </div>
+                
+                <div className='aboutDivider'/>
+                <div className='aboutSection'>
+                  <h2 className='aboutSectionHeader'><span className='lacuna'> Membership</span>  <br/> Price</h2>
+                  <div className="btnContainer">
+                    <button className="boldBtn yellow membershipBtn">1 SEMESTER <br/> $60</button>
+                    <button className="boldBtn yellow membershipBtn">FULL YEAR <br/> $100 </button>
+                    <button className="boldBtn invert membershipBtn">1 OFF CLASS* <br/> $10</button>
+                  </div>
+                  <p>
+                  An AUMT membership gives you access to an 1 hour/1 class per week and discounted prices to our priced events! <br/>
+                  *One off classes are allowed for $10, please message us to book a 1-off session.
+                  </p>
+
+
+                </div>
+                <div className='aboutDivider'/>
+                <div className='aboutSection'>
+                  <h2 className='aboutSectionHeader'><span className='lacuna'> Weekly</span>  <br/> TRAININGS</h2>
+                  <div className='aboutSectionBody'>
+                    <div className='aboutSectionDescription'>
+                      <p>
+                      Trainings consist of up to 40 members. Our trainers and committee are at trainings to help you learn and give personalized feedback, so never hesitate to ask! <br/>
+                      <br/>
+                      Our classes are tailored to different skill levels.<br/>
+                      <br/>
+                      - Beginners Trainings are best for those with little to no Muay Thai experience. Learn the fundamentals of Muay Thai in a low-pressure and friendly environment.<br/>
+                      <br/>
+                      - Intermediate Trainings are best for those who have prior Muay Thai experience and/or approval from committee and coach. A more serious pace, these classes teach more advanced technique and includes sparring.<br/>
+
+                      </p>
+                      <div className="btnContainer">
+                        <button className="boldBtn orange"><a href='/signups'>SIGN UP TO TRAINING</a>  </button>
+                      </div>
+                    </div>
+                    <img  className="aboutSectionImg" src="./photos/content/Trainings.jpg" alt="" />
+                  </div>
+                  
+
+
+                </div>
+                <div className='aboutDivider'/>
+                <div className='aboutSection'>
+                  <h2 className='aboutSectionHeader'><span className='lacuna'> Social</span>  <br/> EVENTS</h2>
+                  <div className='aboutSectionBody'>
+                    <img  className="aboutSectionImg" src="./photos/content/Events.jpg" alt="" />
+                    <div className='aboutSectionDescription'>
+                      <p>
+                        The social events planned by our committee gives our members a chance to bond outside of the gym. <br/>
+                        <br/>
+                        Past events include:<br/>
+                        - Annual Muay Thai Retreat<br/>
+                        - Karaoke Night<br/>
+                        - Collabs with other UoA Clubs<br/>
+                        <br/>
+                        Keep updated on our socials for any events!
+                      </p>
+                    
+                      <div className="btnContainer">
+                        <button className="boldBtn pink"><a href='/signups'>JOIN EVENTS</a>  </button>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  
+
+
+                </div>
+                <div className='aboutDivider'/>
+
+
+                {/* <div className="aboutDescriptionRow">
                     <div className="aboutDescriptionImg">
                         <div className="whatIsMuayThaiPhotoWrapper">
                             <img className='absoluteResponsiveImg' src="./photos/content/muaythai2.jpg" alt="muay thai 2"/>
@@ -96,14 +177,14 @@ export class About extends Component {
                     </div>
                     <div className="clearBoth"></div>
                 </div>
-                <Divider className='aboutDivider'/>
+                <Divider className='aboutDivider'/> */}
                 <div className="aboutFooterNotes">
-                    <h3 className='aboutSectionHead'>Want to know more?</h3>
-                    <p>Check out our <Link to='/faq'>FAQ</Link> page or contact the committee at
+                    <h3 className='aboutSectionHead'>WANT TO KNOW MORE?</h3>
+                    <p>Check out our <Link to='/faq'>FAQ page</Link>, get in touch through
                     <a href='mailto:uoamuaythai@gmail.com'> uoamuaythai@gmail.com </a>
-                    or through&nbsp;
+                    or&nbsp;
                     <Button className='aboutInlineLink' type='link' onClick={Links.openAumtFb}>Facebook </Button>
-                    &nbsp;or&nbsp;
+                    /
                     <Button className='aboutInlineLink' type='link' onClick={Links.openAumtInsta}>Instagram</Button>.
                     </p>
                 </div>
