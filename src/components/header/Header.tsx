5823f657b2c38af42d15cd53d6d60f85c48bb35e
tmacsmee <troymacsmee@gmail.com>
2025-08-02 22:17:53 +1200

    feat: reduce header logo size

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,6 @@ import InstagramFilled from '@ant-design/icons/InstagramFilled'
 import { Link, useLocation } from 'react-router-dom'
 import { useAuth } from '../../context/AuthProvider'
 import { Links } from '../../services/links'
-import { AumtMember } from '../../types'
 import './Header.css'
 import TopMenu from './TopMenu'
 import UserInfo from './UserInfo'
@@ -24,11 +23,10 @@ export default function Header() {
 
   return (
     <header className="headerContainer">
-      <div className="imageContainer">
-        <Link to="/">
-          <img className="logoImg" src={'logos/AUMTLogo.png'} alt="" />
-        </Link>
-      </div>
+      <Link to="/">
+        <img className="logoImg" src={'logos/AUMTLogo.png'} alt="" />
+      </Link>
+
       <TopMenu isAdmin={isAdmin} authedUser={authedUser} />
       <div className="socialsContainer">
         {authedUser ? (
